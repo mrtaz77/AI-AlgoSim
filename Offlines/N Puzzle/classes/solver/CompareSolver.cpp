e0a4045939b300113e051dd6b7629fd20a4291b2
mrtaz77 <mrtazwar23@gmail.com>
2024-09-06 22:35:42 +0600

    this gives less number of explored & expanded nodes

--- Offlines/N Puzzle/classes/solver/CompareSolver.cpp
+++ Offlines/N Puzzle/classes/solver/CompareSolver.cpp
@@ -2,7 +2,7 @@
 
 bool CompareSolver::operator()(Solver *lhs, Solver *rhs) {
 	if (lhs->get_total_cost() == rhs->get_total_cost()) {
-		return lhs->get_step_cost() > rhs->get_step_cost();
+		return lhs->get_heuristic_cost() > rhs->get_heuristic_cost();
 	}
 	return lhs->get_total_cost() > rhs->get_total_cost();
 }
