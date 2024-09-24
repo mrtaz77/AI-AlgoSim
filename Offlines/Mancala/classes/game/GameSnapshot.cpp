9bfac439e39c71385859f5a0f624db7753541ab5
mrtaz77 <mrtazwar23@gmail.com>
2024-09-24 23:14:15 +0600

    implement helpers

--- Offlines/Mancala/classes/game/GameSnapshot.cpp
+++ Offlines/Mancala/classes/game/GameSnapshot.cpp
@@ -22,3 +22,31 @@ ostream& operator<<(ostream& out, const GameSnapshot& snapshot) {
     return out;
 }
 
+void GameSnapshot::makeMove(int pitIndex) {
+    currentTurn->makeMove(*this, pitIndex);
+}
+
+bool GameSnapshot::isPlayerATurn() const {
+    return is_turn_of_A;
+}
+
+bool GameSnapshot::isGameOver() const {
+    int sumA = 0, sumB = 0;
+    for (int i = 0; i < 6; ++i) {
+        sumA += board[i];
+        sumB += board[i + 7];
+    }
+    return sumA == 0 || sumB == 0;
+}
+
+int GameSnapshot::getBin(int binIndex) {
+    return board[binIndex];
+}
+
+int GameSnapshot::getStorageA() const {
+    return board[6];
+}
+
+int GameSnapshot::getStorageB() const {
+    return board[13];
+}
