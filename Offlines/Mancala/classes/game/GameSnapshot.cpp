9d47feb5a8db29c69bc20b068ff7439b761ce328
mrtaz77 <mrtazwar23@gmail.com>
2024-09-25 00:15:24 +0600

    removed state boolean

--- Offlines/Mancala/classes/game/GameSnapshot.cpp
+++ Offlines/Mancala/classes/game/GameSnapshot.cpp
@@ -2,51 +2,59 @@
 
 GameSnapshot::GameSnapshot() {
     board = vector<int>(BOARD_SIZE, INITIAL_NUM_OF_STONES_PER_BIN);
-    board[6] = board[13] = 0;
-    is_turn_of_A = true;
+    board[NUM_OF_BINS_PER_SIDE] = board[BOARD_SIZE - 1] = 0;
+    current_turn = make_unique<PlayerATurn>();
 }
 
-ostream& operator<<(ostream& out, const GameSnapshot& snapshot) {
+ostream& operator<<(ostream& out, const GameSnapshot& game_snap) {
     out << "+-----+-----+-----+-----+-----+-----+-----+-----+\n";
     out << "| ";
     for (int i = 13; i >= 7; --i) {
-        out << std::setw(2) << snapshot.board[i] << "  | ";
+        out << std::setw(2) << game_snap.board[i] << "  | ";
     }
     out << " A  |";
     out << "\n+     +-----+-----+-----+-----+-----+-----+     +\n";
     out << "|  B  | ";
     for (int i = 0; i <= 6; ++i) {
-        out << std::setw(2) << snapshot.board[i] << "  | ";
+        out << std::setw(2) << game_snap.board[i] << "  | ";
     }
     out << "\n+-----+-----+-----+-----+-----+-----+-----+-----+\n";
     return out;
 }
 
-void GameSnapshot::makeMove(int pitIndex) {
-    currentTurn->makeMove(*this, pitIndex);
+void GameSnapshot::make_move(int pitIndex) {
+    current_turn->make_move(*this, pitIndex);
 }
 
-bool GameSnapshot::isPlayerATurn() const {
-    return is_turn_of_A;
+bool GameSnapshot::is_playerA_turn() const {
+    return dynamic_cast<PlayerATurn*>(current_turn.get()) != nullptr;
 }
 
-bool GameSnapshot::isGameOver() const {
+bool GameSnapshot::is_game_over() const {
     int sumA = 0, sumB = 0;
-    for (int i = 0; i < 6; ++i) {
+    for (int i = 0; i < NUM_OF_BINS_PER_SIDE; ++i) {
         sumA += board[i];
-        sumB += board[i + 7];
+        sumB += board[i + NUM_OF_BINS_PER_SIDE + 1];
     }
     return sumA == 0 || sumB == 0;
 }
 
-int GameSnapshot::getStonesInBin(int binIndex) {
-    return board[binIndex];
+int GameSnapshot::get_stones_in_bin(int bin_index) {
+    return board[bin_index];
 }
 
-int GameSnapshot::getStorageA() const {
-    return board[6];
+int GameSnapshot::get_storageA() const {
+    return board[NUM_OF_BINS_PER_SIDE];
 }
 
-int GameSnapshot::getStorageB() const {
-    return board[13];
+int GameSnapshot::get_storageB() const {
+    return board[BOARD_SIZE - 1];
+}
+
+void GameSnapshot::set_stones_in_bin(int bin_index, int stones) {
+    board[bin_index] = stones;
+}
+
+void GameSnapshot::set_turn(unique_ptr<Turn> turn) {
+    current_turn = move(turn);
 }
--- Offlines/Mancala/classes/game/GameSnapshot.hpp
+++ Offlines/Mancala/classes/game/GameSnapshot.hpp
@@ -2,21 +2,22 @@
 
 #include <bits/stdc++.h>
 #include "../turn/Turn.hpp"
+#include "../turn/PlayerATurn.hpp"
 #include "../constants/Constants.hpp"
 using namespace std;
 
 class GameSnapshot {
     vector<int> board;
-    bool is_turn_of_A;
-    unique_ptr<Turn> currentTurn;
+    unique_ptr<Turn> current_turn;
 public:
     GameSnapshot();
     friend ostream& operator<<(ostream&, const GameSnapshot&);
-    void makeMove(int);
-    void setTurn();
-    bool isPlayerATurn() const;
-    bool isGameOver() const;
-    int getBin(int);
-    int getStorageA() const;
-    int getStorageB() const;
+    void make_move(int);
+    bool is_playerA_turn() const;
+    void set_turn(unique_ptr<Turn>);
+    bool is_game_over() const;
+    int get_stones_in_bin(int);
+    void set_stones_in_bin(int, int);
+    int get_storageA() const;
+    int get_storageB() const;
 };
