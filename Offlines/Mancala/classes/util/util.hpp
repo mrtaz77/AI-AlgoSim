42ad5131d6c209850ae22cd4634041c43e0f135d
mrtaz77 <mrtazwar23@gmail.com>
2024-09-26 22:52:59 +0600

    move constants to util

--- Offlines/Mancala/classes/util/util.hpp
+++ Offlines/Mancala/classes/util/util.hpp
@@ -1,9 +1,17 @@
 #pragma once
+
+#define NUM_OF_BINS_PER_SIDE 6
+#define NUM_OF_PLAYERS 2
+#define TOTAL_NUM_OF_BINS (NUM_OF_BINS_PER_SIDE * NUM_OF_PLAYERS)
+#define NUM_OF_STORAGE NUM_OF_PLAYERS
+#define BOARD_SIZE (TOTAL_NUM_OF_BINS + NUM_OF_STORAGE)
+#define INITIAL_NUM_OF_STONES_PER_BIN 4
+
 #include <string>
-#include "../game/modes/Mode.hpp"
+#include "../game/modes/Modes.hpp"
 #include "../heuristics/Heuristic.hpp"
 using namespace std;
 
 int validate_move(string);
-enum Modes validate_mode_choice(string);
-enum Heuristics validate_heuristic_choice(string);
+Modes validate_mode_choice(string);
+Heuristics validate_heuristic_choice(string);
