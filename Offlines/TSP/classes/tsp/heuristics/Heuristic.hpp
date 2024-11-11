563a10b59e40bb60757c2df8f322c20e90694ae0
mrtaz77 <mrtazwar23@gmail.com>
2024-11-11 16:02:34 +0600

    fix return type

--- Offlines/TSP/classes/tsp/heuristics/Heuristic.hpp
+++ Offlines/TSP/classes/tsp/heuristics/Heuristic.hpp
@@ -15,7 +15,7 @@ public:
     Heuristic(Graph&);
     virtual ~Heuristic() = default;
     virtual void solve() = 0;
-    virtual const string& get_heuristic_name() const = 0;
+    virtual const string get_heuristic_name() const = 0;
     vector<int>& get_tour();
     long double get_time_taken() const;
     long double get_tour_cost() const;
