20e4b8819ceb48c1e04760d4f35f75d071c39a41
mrtaz77 <mrtazwar23@gmail.com>
2024-11-11 10:31:50 +0600

    add heuristic name as virtual method

--- Offlines/TSP/classes/tsp/heuristics/Heuristic.hpp
+++ Offlines/TSP/classes/tsp/heuristics/Heuristic.hpp
@@ -11,13 +11,14 @@ protected:
     vector<int> tour;
     long double tour_cost;
     long double time_taken;
-
 public:
-    Heuristic(Graph& graph);
+    Heuristic(Graph&);
 
     virtual ~Heuristic() = default;
 
     virtual void solve() = 0;
+
+    virtual const string& get_heuristic_name() const = 0;
 
     vector<int>& get_tour();
     long double get_time_taken() const;
