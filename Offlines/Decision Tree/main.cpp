d390e2bfbecdac33aeb9a2e56c342497fa2c0c67
mrtaz77 <mrtazwar23@gmail.com>
2024-12-05 01:17:07 +0600

    update Makefile and main.cpp to include Dataset class and constants; modify run command to accept number of runs

--- Offlines/Decision Tree/main.cpp
+++ Offlines/Decision Tree/main.cpp
@@ -3,15 +3,17 @@
 #include "classes/validator/FileExists.hpp"
 #include "classes/validator/RowValidator.hpp"
 #include "classes/validator/Validator.hpp"
+#include "classes/Dataset.hpp"
+#include "classes/DatasetConstants.hpp"
 
 using namespace std;
 
 int 
 main(int argc, char* argv[])
 {
-  if(argc != 2)
+  if(argc != 3)
   {
-    cout << "Usage: " << argv[0] << " <input file>" << endl;
+    cout << "Usage: " << argv[0] << " <input file> <number of runs>" << endl;
     return 1;
   }
 
@@ -23,10 +25,29 @@ main(int argc, char* argv[])
   file_check->set_next(column_check);
   column_check->set_next(row_check);
 
-  if (file_check->validate(file_path))
+  if (!file_check->validate(file_path))
   {
-    cout << "Validation successful." << endl;
-    return 0;
+    cout << "Invalid file." << endl;
+    return 1;
   }
-  return 1;
+
+  Dataset dataset;
+  ifstream file(file_path);
+
+  file >> dataset;
+
+  int runs = stoi(argv[2]);
+
+  if(runs < 1)
+  {
+    cout << "Number of runs must be greater than 0." << endl;
+    return 1;
+  }
+
+  for(int i = 0; i < runs; i++)
+  {
+    auto [train, test] = dataset.split(TRAINING_SPLIT_RATIO);
+  }
+
+  return 0;
 }
