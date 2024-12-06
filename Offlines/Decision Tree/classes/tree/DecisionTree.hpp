b08e8b173e143a8ddf722a9c70e25ae968d629e1
mrtaz77 <mrtazwar23@gmail.com>
2024-12-06 14:41:18 +0600

    for printing tree

--- Offlines/Decision Tree/classes/tree/DecisionTree.hpp
+++ Offlines/Decision Tree/classes/tree/DecisionTree.hpp
@@ -9,4 +9,5 @@ class DecisionTree
 public:
   virtual string classify(const vector<string>& instance) const = 0;
   virtual ~DecisionTree() = default;
+  virtual void print(ostream&, int) const = 0;
 };
